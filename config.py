628fc179f2cc4d84fd914517c1905dbcc5c07a31
pydevveloper <verpatoffa@yandex.ru>
2025-01-26 18:16:55 +0600

    2stand

--- config.py
+++ config.py
@@ -1,27 +1,26 @@
 from aiogram import Bot
 from aiogram import Dispatcher
 from aiogram.client.default import DefaultBotProperties
-# from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings
 
-#
-#
-# class EnvSettings(BaseSettings):
-#     """
-#     Environment settings.
-#     """
-#
-#     class Config:
-#         env_file = ".env"
-#         env_file_encoding = "utf-8"
-#         extra = "ignore"
-#
-#
-# class BotSettings(EnvSettings):
-#     token: str
-#     admin_id: str
-#
-#
-# bot_settings = BotSettings()
+
+
+class EnvSettings(BaseSettings):
+    """
+    Environment settings.
+    """
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        extra = "ignore"
+
+
+class BotSettings(EnvSettings):
+    token: str
+
+
+bot_settings = BotSettings()
 default = DefaultBotProperties(parse_mode='HTML', protect_content=False)
-bot = Bot(token = '7925804288:AAFNw_AHCGVEVeccQgDrYz3zK1UPLPyPOoU', default=default)
+bot = Bot(token = bot_settings.token, default=default)
 dp = Dispatcher()
